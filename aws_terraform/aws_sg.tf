5a917de569977b6f51784a273aea10d72ecd1c2e
Hikosaburou <eichan.yazawa@gmail.com>
2020-04-13 11:54:13 +0900

    fix: VPC DNS有効化

--- aws_terraform/aws_sg.tf
+++ aws_terraform/aws_sg.tf
@@ -12,6 +12,14 @@ resource "aws_security_group" "ec2_instance" {
     cidr_blocks = [var.my_ip]
   }
 
+  ingress {
+    description = "SSH from My IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
