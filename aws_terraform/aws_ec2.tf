31c37d012a10719bfd1fd98c8d6f5f5cd1137d24
Hikosaburou <eichan.yazawa@gmail.com>
2020-04-13 10:11:36 +0900

    feat: tf EC2インスタンス起動

---
+++ aws_terraform/aws_ec2.tf
@@ -0,0 +1,37 @@
+data "aws_ami" "amazonlinux2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  owners = ["137112412989"]
+}
+
+resource "aws_instance" "default_wordpress" {
+  ami                    = data.aws_ami.amazonlinux2.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.main["public-a"].id
+  vpc_security_group_ids = [aws_security_group.ec2_instance.id]
+  user_data              = file("wordpress_installation.txt")
+
+  tags = {
+    Name = "${var.project_key}-wordpress"
+  }
+}
